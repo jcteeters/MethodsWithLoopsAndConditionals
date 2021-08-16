1c4c29572b991167b0ba7764051ae300d2d6a904
Jacob Teeters <jcteeters7@gmail.com>
2021-08-16 15:48:03 -0500

    message

---
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodsWithLoopsAndConditionals
+{
+    class Program
+    {
+        //1. Write a method that will print to the console all numbers 1000 through -1000
+        public static void Print1000s()
+        {
+            for (int i = 1000; i >= 1000; i--)
+            {
+                Console.Write(i);
+            }
+        }
+        //2. Write a method that will print to the console numbers 3 through 999 by 3 each time
+        public static void
+        //3. Write a method to accept two integers as parameters and check whether they are equal or not
+
+        static void Main(string[] args)
+        {
+         
+        }
+    }
+}
